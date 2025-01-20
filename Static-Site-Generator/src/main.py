615008efd3d2972b95e5645f32617a90a5e1ba5c
Zoisit <smaragdeule@gmx.de>
2025-01-20 23:24:41 +0000

    C5-L1: copy static to public folder

--- Static-Site-Generator/src/main.py
+++ Static-Site-Generator/src/main.py
@@ -1,9 +1,31 @@
 from textnode import TextNode, TextType
-from htmlnode import HTMLNode
-from leafnode import LeafNode
+from os import listdir, path, mkdir
+import shutil
+
+## dangerous
+def copy_dir(source, destination):
+    print(f"Trying to copy from {source} to {destination}...")
+    if "..\\" in destination:
+        raise Exception("Are you sure you're allowed to do that?")
+    
+    if path.exists(destination):
+        shutil.rmtree(destination)
+    mkdir(destination)
+
+    # use recursion for every nested directory
+    for dir in listdir(source):
+        if not path.isfile(path.join(source, dir)):
+            copy_dir(path.join(source, dir), path.join(destination, dir))
+        else:
+            shutil.copy(path.join(source, dir), destination)
+
+    print(f"...fninished copying from {source} to {destination}.")
+
 
 def main():
     testNode = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(testNode)
+
+    copy_dir("static", "public")
 
 main()
